17ac678e54e8277a23b6e61e68bb71bad2d77539
mohammd2005x-boop <mohammd2005x@gmail.com>
2026-04-29 03:49:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('action-btn');
+btn.onclick=function() {
+    btn.innerHTML="تم الضغط!";
+    btn.style.backgroundColor ="#2ecc71";
+    alert("رائع لقد تفاعلة مع الموقع.");
+};
+
